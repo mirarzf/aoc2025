d11ed41d6bb9835b7ea58dfb97fc8132522ef94d
mirar <mira.razaf@gmail.com>
2025-12-05 08:17:52 +0100

    Initialization of day 5

--- pysolutions/day05/day05.py
+++ pysolutions/day05/day05.py
@@ -1,11 +1,38 @@
 import numpy as np 
 
+def isFresh(fresh_ranges, id): 
+    for rangeids in fresh_ranges: 
+        if id >= rangeids[0] and id <= rangeids[1]: 
+            return True 
+    return False 
+
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
     lines = f.readlines()
     f.close()
 
-    return 0
+    i = 0 
+    line = lines[0]
+    fresh_ranges = []
+    while line != '\n': 
+        ids_ranges = [int(e) for e in line.split('-')] 
+        fresh_ranges.append(ids_ranges)
+        i += 1 
+        line = lines[i]
+    
+    i += 1 
+    ids_to_check = []
+    while i < len(lines) and lines[i] != '\n': 
+        ids_to_check.append(int(lines[i]))
+        i += 1 
 
-# Part 1: 1527 
-# Part 2: 8690 
+    print(fresh_ranges, ids_to_check)
+
+    counter = 0 
+    for id in ids_to_check: 
+        if isFresh(fresh_ranges, id): 
+            counter += 1 
+
+    return counter 
+# Part 1: 
+# Part 2: 
