f67b003c3c28c4c1e4524fde5c1cd77edfaf15d3
mirar <mira.razaf@gmail.com>
2025-12-05 08:09:31 +0100

    Day 5 initialization

---
+++ pysolutions/day05/day05.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+def solve(inputfile, puzzlepart): 
+    f = open(inputfile, 'r')
+    lines = f.readlines()
+    f.close()
+
+    return 0
+
+# Part 1: 1527 
+# Part 2: 8690 
