8b3ff17dba1bd701a70e99a94fc5bd012f60d34e
mirar <mira.razaf@gmail.com>
2025-12-05 21:34:30 +0100

    Updated to correct solution part 1 of day 5

--- pysolutions/day05/day05.py
+++ pysolutions/day05/day05.py
@@ -3,8 +3,8 @@ import numpy as np
 def isInRanges(fresh_ranges, id): 
     for rangeids in fresh_ranges: 
         if id >= rangeids[0] and id <= rangeids[1]: 
-            return True, rangeids
-    return False, [0,0]
+            return True 
+    return False 
 
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
@@ -29,25 +29,25 @@ def solve(inputfile, puzzlepart):
     if puzzlepart == 1: 
         counter = 0 
         for id in ids_to_check: 
-            if isInRanges(fresh_ranges, id)[0]: 
+            if isInRanges(fresh_ranges, id): 
                 counter += 1 
 
         return counter 
 
-    new_fresh_ranges = []
-    for fresh_range in fresh_ranges: 
-        new_range = []
-        first_id, sec_id = fresh_range[0], fresh_range[1]
-        isInRangeFID = isInRanges(new_fresh_ranges, first_id)
-        isInRangeSID = isInRanges(new_fresh_ranges, sec_id)
-        if isInRangeFID[0] and not isInRangeSID[0]: 
-            new_range = [first_id, isInRangeSID[1][0]-1] 
-        if not isInRangeFID[0] and isInRangeSID[0]: 
-            new_range = [isInRangeSID[1][1]+1, sec_id] 
-        if not isInRangeFID[0] and not isInRangeSID[0]: 
-            new_range = [first_id, sec_id] 
-        if len(new_range) != 0: 
-            new_fresh_ranges.append(new_range)
+    # new_fresh_ranges = []
+    # for fresh_range in fresh_ranges: 
+    #     new_range = []
+    #     first_id, sec_id = fresh_range[0], fresh_range[1]
+    #     isInRangeFID = isInRanges(new_fresh_ranges, first_id)
+    #     isInRangeSID = isInRanges(new_fresh_ranges, sec_id)
+    #     if isInRangeFID[0] and not isInRangeSID[0]: 
+    #         new_range = [first_id, isInRangeSID[1][0]-1] 
+    #     if not isInRangeFID[0] and isInRangeSID[0]: 
+    #         new_range = [isInRangeSID[1][1]+1, sec_id] 
+    #     if not isInRangeFID[0] and not isInRangeSID[0]: 
+    #         new_range = [first_id, sec_id] 
+    #     if len(new_range) != 0: 
+    #         new_fresh_ranges.append(new_range)
         
     # Start counting all the ids 
     somme = 0 
