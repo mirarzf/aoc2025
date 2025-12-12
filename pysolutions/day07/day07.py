ccbdbd560c4aca3a61f2b80d87c3cc5413109ff8
mirar <mira.razaf@gmail.com>
2025-12-12 22:40:27 +0100

    Day 7 code is less redundant

--- pysolutions/day07/day07.py
+++ pysolutions/day07/day07.py
@@ -1,29 +1,14 @@
 import numpy as np 
 
-def getSplittingNb(grid, entryCoords):
+def getNbPaths(grid, dist, puzzlepart=1):
     if grid.shape[0] == 1: 
-        return 0
-    
-    nextCoords = [] 
-    nb_splits = 0 
-    for j in entryCoords: 
-        if grid[0,j] == 1: 
-            if j-1 >= 0: 
-                nextCoords.append(j-1)
-            if j+1 < grid.shape[1]: 
-                nextCoords.append(j+1)
-            nb_splits += 1 
-        else: 
-            nextCoords.append(j)
-    nextCoords = list(set(nextCoords))
-
-    return nb_splits + getSplittingNb(grid[1:,:], nextCoords)  
-
-def getNbTimelines(grid, dist): 
-    if grid.shape[0] == 1: 
-        return np.sum(dist)
+        if puzzlepart == 1: 
+            return 0
+        else: # puzzlepart == 2 
+            return np.sum(dist)
     
     next_dist = np.zeros(grid.shape[1], dtype=int)
+    nb_splits = 0 
     for j in range(grid.shape[1]): 
         if dist[j] > 0: 
             if grid[0,j] == 1: 
@@ -31,11 +16,14 @@ def getNbTimelines(grid, dist):
                     next_dist[j-1] += dist[j]
                 if j+1 < grid.shape[1]: 
                     next_dist[j+1] += dist[j]
+                nb_splits += 1 
             else: 
                 next_dist[j] += dist[j]
     
-    return getNbTimelines(grid[1:], next_dist)
-
+    if puzzlepart == 1: 
+        return nb_splits + getNbPaths(grid[1:], next_dist, puzzlepart)
+    else: 
+        return getNbPaths(grid[1:], next_dist, puzzlepart)
 
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
@@ -48,16 +36,17 @@ def solve(inputfile, puzzlepart):
     for i in range(grid.shape[0]): 
         for j in range(grid.shape[1]): 
             if lines[i][j] == 'S': 
-                Sidx = i, j 
+                Sidx = j 
             elif lines[i][j] == '^': 
                 grid[i][j] = 1 
     
+    dist = np.zeros(grid.shape[1], dtype=int)
+    dist[Sidx] = 1
+
     if puzzlepart == 1: 
-        return getSplittingNb(grid, [Sidx[1]])
+        return getNbPaths(grid, dist, 1)
     else: 
-        dist = np.zeros(grid.shape[1], dtype=int)
-        dist[Sidx[1]] = 1
-        return getNbTimelines(grid, dist)
+        return getNbPaths(grid, dist, 2)
 
 # Part 1: 1649 
-# Part 2: 
+# Part 2: 16937871060075
