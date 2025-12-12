a8b91694ee780ec4a6df11f7341c020de89d79a4
mirar <mira.razaf@gmail.com>
2025-12-12 19:53:36 +0100

    Day 7 part 1 python solution

--- pysolutions/day07/day07.py
+++ pysolutions/day07/day07.py
@@ -1,12 +1,42 @@
+import numpy as np 
+
+def getSplittingNb(grid, entryCoords):
+    if grid.shape[0] == 1: 
+        return 0
+    
+    nextCoords = [] 
+    nb_splits = 0 
+    for j in entryCoords: 
+        if grid[0,j] == 1: 
+            if j-1 >= 0: 
+                nextCoords.append(j-1)
+            if j+1 < grid.shape[1]: 
+                nextCoords.append(j+1)
+            nb_splits += 1 
+        else: 
+            nextCoords.append(j)
+    nextCoords = list(set(nextCoords))
+
+    return nb_splits + getSplittingNb(grid[1:,:], nextCoords)        
+
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
     lines = f.readlines()
     f.close()
     
-    
+    grid = np.zeros((len(lines), len(lines[0])), dtype=int)
 
-    total = 0 
-    return total
-            
+    Sidx = 0, 0 
+    for i in range(grid.shape[0]): 
+        for j in range(grid.shape[1]): 
+            if lines[i][j] == 'S': 
+                Sidx = i, j 
+            elif lines[i][j] == '^': 
+                grid[i][j] = 1 
+    
+    retval = getSplittingNb(grid, [Sidx[1]])
+    
+    return retval
+
 # Part 1: 
 # Part 2: 
