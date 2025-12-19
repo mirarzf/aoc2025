90694e8b58aaaf27c646a9f8ca1ebff207d35325
mira <mira.razaf@gmail.com>
2025-12-19 10:44:59 +0100

    Updated day 8 python solution for both parts

--- pysolutions/day08/day08.py
+++ pysolutions/day08/day08.py
@@ -3,16 +3,6 @@ import numpy as np
 def eudist(v1, v2): 
     return np.sum([(v2[i]-v1[i])**2 for i in range(3)])
 
-def appendSorted(liste, newelement, lenlistemax = None): 
-    if lenlistemax == None: 
-        lenlistemax = len(liste)+1
-    if len(liste) == 0 or newelement[0] <= liste[0][0]: 
-        return [newelement] + liste[:lenlistemax-1]
-    for i in range(len(liste)-1): 
-        if liste[i][0] <= newelement[0] and liste[i+1][0] >= newelement[0]: 
-            return liste[:i+1] + [newelement] + liste[i+1:lenlistemax-1]
-    return liste 
-
 def solve(inputfile, puzzlepart): 
     f = open(inputfile, 'r')
     lines = f.readlines()
@@ -25,25 +15,26 @@ def solve(inputfile, puzzlepart):
     vertices = np.array(vertices)
 
     all_dists = []
-    if puzzlepart == 1: 
-        n_connexions = 1000
-    else: 
-        n_connexions = None
     index = 0
     for i in range(vertices.shape[0]): 
         for j in range(i+1, vertices.shape[0]): 
             dist = eudist(vertices[i], vertices[j])
-            all_dists = appendSorted(all_dists, (dist, i, j), lenlistemax=n_connexions)
+            all_dists.append((dist, i, j))
             index += 1 
     
-    # print(all_dists)
-    print(len(all_dists))
+    if puzzlepart == 1: 
+        n_connexions = 1000
+    else: 
+        n_connexions = len(all_dists)
+    all_dists = np.array(all_dists)
+    all_dists = all_dists[all_dists[:,0].argsort()]
+    all_dists = all_dists[:n_connexions]
 
     components = {}
     circuits_lengths = {0:1}
     i_dist = 0 
-    continueWhileLoop = True
-    while i_dist < len(all_dists) and continueWhileLoop: 
+    max_circuit = 0
+    while i_dist < len(all_dists) and max_circuit < len(vertices): 
         e = all_dists[i_dist] 
         i, j = e[1], e[2]
         if i in components.keys() and j not in components.keys(): 
@@ -64,6 +55,8 @@ def solve(inputfile, puzzlepart):
                         components[vertex] = new_circuit_r 
 
                 circuits_lengths[new_circuit_r] += circuits_lengths[ex_circuit_r]
+                if max_circuit < circuits_lengths[new_circuit_r]: 
+                    max_circuit = circuits_lengths[new_circuit_r]
                 circuits_lengths[ex_circuit_r] = 0 
             
         else: # i not in components.keys() and j not in components.keys()
@@ -71,7 +64,6 @@ def solve(inputfile, puzzlepart):
             components[j] = i
             circuits_lengths[i] = 2
         
-        continueWhileLoop = puzzlepart == 1 or (max(circuits_lengths.values()) < len(vertices))
         i_dist += 1 
     
     if puzzlepart == 1: 
@@ -80,16 +72,14 @@ def solve(inputfile, puzzlepart):
         circuits_lengths_liste.sort(reverse=True)
         for e in circuits_lengths_liste[:3]: 
             retval *= e
-        print(circuits_lengths_liste[:3])
         
         return retval
 
     else: 
-        u = all_dists[i_dist-1][1]
-        v = all_dists[i_dist-1][2]
-        print(vertices[u], vertices[v])
+        u = int(all_dists[i_dist-1][1])
+        v = int(all_dists[i_dist-1][2])
         retval = vertices[u][0]*vertices[v][0]
         return retval 
 
 # Part 1: 330786
-# Part 2: 
+# Part 2: 3276581616
